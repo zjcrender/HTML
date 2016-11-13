5b3d04a5fba99e368843fe962e556e2a2da6ebbb
朱继昌 <zjc_daniel@163.com>
2016-11-13 10:33:51 +0800

    随机运动小球

---
+++ RandomBall/Ball.js
@@ -0,0 +1,60 @@
+/**
+ * Created by Administrator on 2016/8/23 0023.
+ */
+
+window.ucai = window.ucai || {};
+
+(function () {
+
+    function Ball(color) {
+        this._htmlNode = document.createElement("div");
+        this._color = color;
+    }
+
+    var pro = Ball.prototype;
+
+    pro.getHtmlNode = function () {
+        this.ballStyle();
+        this.randomPosition();
+        this.move();
+        return this._htmlNode;
+    }
+
+    pro.ballStyle = function () {
+        this._htmlNode.style.width = "40px";
+        this._htmlNode.style.height = "40px";
+        this._htmlNode.style.backgroundColor = this._color;
+        this._htmlNode.style.borderRadius = "50%";
+        this._htmlNode.style.position = "absolute";
+    }
+
+    pro.randomPosition = function () {
+        this._htmlNode.style.top = Math.floor(Math.random() * 700) + "px";
+        this._htmlNode.style.left = Math.floor(Math.random() * 1400) + "px";
+    }
+
+    pro.move = function () {
+        var nowX = parseInt(this._htmlNode.style.left);
+        var nowY = parseInt(this._htmlNode.style.top);
+        var speedX, speedY;
+        while (true) {
+            speedX = 10 - Math.floor(Math.random() * 21);
+            speedY = 10 - Math.floor(Math.random() * 21);
+            if (!(speedX == 0 && speedY == 0)) break;
+        }
+
+        var id = setInterval(function () {
+            nowX += speedX, nowY += speedY;
+            this._htmlNode.style.left = nowX + "px";
+            this._htmlNode.style.top = nowY + "px";
+            if ((nowX <= -40 || nowX >= document.body.clientWidth-40) || (nowY <= -40 || nowY >= window.screen.height-40)) {
+                clearInterval(id);
+                this._htmlNode.parentNode.removeChild(this._htmlNode);
+            }
+        }.bind(this), 20)
+    }
+
+
+    ucai.Ball = Ball;
+
+})();
---
+++ RandomBall/main.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Administrator on 2016/8/23 0023.
+ */
+
+
+(function () {
+
+    var ballColor = ["#6be","#333","#ffe957","#f29f3f"];
+    var ballColorLength = ballColor.length;
+
+    function creatBall(Num) {
+        for (var i=0;i<Num;i++) {
+            var randomNum = Math.floor(Math.random()*ballColorLength);
+            var ball = new ucai.Ball(ballColor[randomNum]);
+            document.body.appendChild(ball.getHtmlNode());
+        }
+    }
+
+    creatBall(40);
+
+
+
+})();
